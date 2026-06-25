871941beeece3993641a7fed5b2f7cb70badb432
harikrik007 <iamharihk@gmail.com>
2026-06-25 10:22:26 +0530

    minor default prompt change correction

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_PROMPT = 'Describe the video in fine graineddetail.'
+export const DEFAULT_PROMPT = 'Describe the video in fine grained detail.'
 
 export const VLLM_MODEL = 'vllm_model'
